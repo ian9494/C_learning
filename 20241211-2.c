21374c9831b539bc6178fbc79601062ca70566e9
ian9494 <ian20050904@gmail.com>
2024-12-11 09:47:24 +0800

    week14-2
    Q2

--- 20241211-2.c
+++ 20241211-2.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <ctype.h> // 使用 isalpha 函數
 
 int main()
 {
+    char n[1000000];
+
+    while (fgets(n, sizeof(n), stdin) != NULL)
+    {
+        int count = 0;
+        int in_word = 0;
+
+        for (int i = 0; n[i] != '\0'; i++)
+        {
+            if (isalpha(n[i])) // check alphabet
+            {
+                if (!in_word) // check if new word
+                {
+                    count++;
+                    in_word = 1;
+                }
+            }
+            else // cancel if not alphabet
+            {
+                in_word = 0;
+            }
+        }
+
+        printf("%d\n", count);
+    }
+
     return 0;
 }
