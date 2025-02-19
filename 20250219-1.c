13ba41de991de13779573430382fea10e3e19c7e
ian9494 <ian20050904@gmail.com>
2025-02-19 17:39:54 +0800

    week2-2
    補註解

--- 20250219-1.c
+++ 20250219-1.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 
+// 函數：列印所有成績
 void printGrade(int *grade)
 {
+    // 列印六個成績
     printf("%d %d %d %d %d %d\n", grade[0], grade[1], grade[2], grade[3], grade[4], grade[5]);
 }
 
+// 函數：計算並列印平均成績
 void printAverage(int *grade)
 {
     int sum = 0;
+    // 計算六個成績的總和
     for (int i = 0; i < 6; i++)
     {
         sum += grade[i];
     }
+    // 列印平均成績，保留兩位小數
     printf("%.2f\n", (float)sum / 6);
 }
 
+// 函數：找出並列印最低成績
 void findMin(int *grade)
 {
     int min = grade[0];
+    // 找出最小的成績
     for (int i = 1; i < 6; i++)
     {
         if (grade[i] < min)
@@ -25,12 +32,15 @@ void findMin(int *grade)
             min = grade[i];
         }
     }
+    // 列印最低成績
     printf("%d\n", min);
 }
 
+// 函數：找出並列印最高成績
 void findMax(int *grade)
 {
     int max = grade[0];
+    // 找出最大的成績
     for (int i = 1; i < 6; i++)
     {
         if (grade[i] > max)
@@ -38,29 +48,35 @@ void findMax(int *grade)
             max = grade[i];
         }
     }
+    // 列印最高成績
     printf("%d\n", max);
 }
 
+// 函數：退出程式
 void exitProgram(int *grade)
 {
     return;
 }
 
-
 int main()
 {
-    int grade[6];
-    int funcIndex = 0;
+    int grade[6]; // 存放六個成績的陣列
+    int funcIndex = 0; // 存放使用者選擇的功能索引
 
-    void (*func[5])(int) = {printGrade, findMin, findMax, printAverage, exitProgram};
+    // 函數指標陣列，對應不同的功能
+    void (*func[5])(int *) = {printGrade, findMin, findMax, printAverage, exitProgram};
 
     while (1)
     {
+        // 從使用者輸入六個成績
         scanf("%d %d %d %d %d %d", &grade[0], &grade[1], &grade[2], &grade[3], &grade[4], &grade[5]);
+        // 從使用者輸入功能索引
         scanf("%d", &funcIndex);
+        // 如果功能索引在有效範圍內，執行對應的函數
         if (funcIndex >= 0 && funcIndex <= 4)
         {
             func[funcIndex](grade);
+            // 如果選擇退出程式，跳出迴圈
             if (funcIndex == 4)
             {
                 break;
@@ -68,6 +84,7 @@ int main()
         }
         else
         {
+            // 如果功能索引無效，跳出迴圈
             break;
         }
     }
--- 20250219-2.c
+++ 20250219-2.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 
+// 函數：計算並列印圓周長
 void calculateCircumference(float radius)
 {
+    // 圓周長公式：2 * π * 半徑
     printf("%.2f\n", 2 * 3.14 * radius);
 }
 
+// 函數：計算並列印圓面積
 void calculateArea(float radius)
 {
+    // 圓面積公式：π * 半徑 * 半徑
     printf("%.2f\n", 3.14 * radius * radius);
 }
 
+// 函數：計算並列印球體積
 void calculateVolume(float radius)
 {
+    // 球體積公式：4/3 * π * 半徑^3
     printf("%.2f\n", 4.0 / 3.0 * 3.14 * radius * radius * radius);
 }
 
+// 函數：退出程式
 void exitProgram(float radius)
 {
     return;
@@ -22,30 +29,35 @@ void exitProgram(float radius)
 
 int main()
 {
-    float radius;
-    int option;
+    float radius; // 存放半徑的變數
+    int option; // 存放使用者選擇的功能選項
 
+    // 函數指標陣列，對應不同的功能
     void (*funcPtr[4])(float) = {calculateCircumference, calculateArea, calculateVolume, exitProgram};
     
     while (1)
     {
+        // 從使用者輸入功能選項
         scanf("%d", &option);
+        // 如果功能選項在有效範圍內，執行對應的函數
         if (option >= 0 && option <= 3)
         {
+            // 如果選擇退出程式，跳出迴圈
             if (option == 3)
             {
                 break;
             }
+            // 從使用者輸入半徑
             scanf("%f", &radius);
+            // 執行對應的函數
             funcPtr[option](radius);
-            
         }
         else
         {
+            // 如果功能選項無效，跳出迴圈
             break;
         }
     }
-    
     
     return 0;
 }
